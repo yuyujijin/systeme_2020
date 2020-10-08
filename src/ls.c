ebab9dd31c703b08b807ff4516a4c4f00e305e38
adam <adam000202@gmail.com>
2020-10-08 14:01:06 +0200

    Write is now used instead of printf

--- src/ls.c
+++ src/ls.c
@@ -11,29 +11,49 @@ int ls(char *path,int option){//option=1 --> ls -l     option=0 --> ls
   struct dirent *entry;
   while((entry=readdir(dir))!=NULL){//for every entry in the directory
     if(strcmp(entry->d_name,".")!=0&&strcmp(entry->d_name,"..")!=0){// We don't deal with directories '.' and '..'
-      if(option==0)printf(" %s ",entry->d_name);//if we called "ls" then we display all the file name in a single row
-      else{
+      if(option==0){
+        if(write(1,entry->d_name,strlen(entry->d_name))<0)return -1;//if we called "ls" then we display all the file name in a single row
+        if(write(1," ",1)<0)return -1;
+      }else{
         //else we display it like the commande ls -l does
+        // if(chdir(path)<0){
+        //   perror(path);
+        // }
         struct stat info;
-        if(stat(entry->d_name,&info)<0){
-          perror(entry->d_name);
+        char stat_path[strlen(path)+strlen(entry->d_name)];
+        snprintf(stat_path,strlen(path)+strlen(entry->d_name)+2,"%s/%s",path,entry->d_name);
+        if(stat(stat_path,&info)<0){
+          perror(stat_path);
           return -1;
         }
         struct group *gid=getgrgid(info.st_gid);//to get the gid name because info.st_gid only gives us an int
         struct passwd *uid=getpwuid(info.st_uid);//same
-        struct tm *date=gmtime(&info.st_ctime);//to get the date displaied as such: mmm ddd hh:minmin from st_ctime because st_ctime is a long int
-        printf("%o %ld %s %s %ld %s. %d:%d %s",info.st_mode,info.st_nlink,uid->pw_name,gid->gr_name,info.st_size,month[date->tm_mon],date->tm_hour,date->tm_min,entry->d_name);
-        printf("\n");
+        struct tm *date=gmtime(&info.st_mtime);//to get the date displaied as such: mmm ddd hh:minmin from st_ctime because st_ctime is a long int
+        //(int)log10(info.st_size) gives us the number of digit of st_size in base 10;
+        int size=7+2+strlen(uid->pw_name)+strlen(gid->gr_name)+nbDigit(info.st_size)+3+2+2+strlen(entry->d_name);
+        char line[size+10];//we store the line to be written and add +10 for every space etc
+
+        if(snprintf(line,sizeof(line),"%08o %ld %s %s %ld %s. %02d:%02d %s", //we write the data from stat on the line
+          info.st_mode,info.st_nlink,uid->pw_name,gid->gr_name,
+          info.st_size,month[date->tm_mon],date->tm_hour,date->tm_min,entry->d_name)<0)return-1;
+        if(write(1,line,size+10)<0)return -1;
+        if(write(1,"\n",1)<0)return -1;
       }
     }
   }
-  printf("\n");
+  if(write(1,"\n",1)<0)return -1;
   free(entry);
-  closedir(dir);
+  if(closedir(dir)<0){
+    perror(path);
+    return -1;
+  }
   return 0;
 }
-
-ls_tar(char *path,int option){//we use this function to deal with tar file
+int nbDigit (int n) {//return the number of digit of a certain integer. We need it for info.st_size
+    if (n == 0) return 1;
+    return floor (log10 (abs (n))) + 1;
+}
+void ls_tar(char *path,int option){//we use this function to deal with tar file
   /*
     TO DO :
       use the struct header of a tar file done in tp1 with low-level programming
--- src/ls.h
+++ src/ls.h
@@ -7,4 +7,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
 int ls(char *path,int option);
+int nbDigit(int n);
+void ls_tar(char *path,int option);
