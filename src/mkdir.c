098f0e89dbffcd7b214422eb2bc38602c629ef73
Elsa <elsasantosduarte@gmail.com>
2020-10-09 17:01:24 +0200

    ajout mkdir.c

---
+++ src/mkdir.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+
+int mkDirectory(const char* argv)
+{
+  char path[PATH_MAX+1];//directory path
+  snprintf(path, PATH_MAX + 1,"%s",argv);
+
+  struct stat stDir;
+  if(stat(path, &stDir) !=-1)//if the directory exists
+    {
+      perror("mkdir");
+      exit(EXIT_FAILURE);
+    }
+
+  printf("%ld\n",stDir.st_ino);
+  /*if (stat(path, &stDir) == -1) {
+    if(mkdir(path, 0755) == -1)
+      perror("mkdir");
+      / }*/
+  return 0;
+
+}
+
+int main(int argc, const char** argv)
+{
+  mkDirectory(argv[1]);
+  return 0;
+}
