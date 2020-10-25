b4da3bf273db441afc44d9f07211a1333f592d92
Eugene VALTY <eugenevalty@gmail.com>
2020-10-25 23:43:47 +0100

    /

--- src/shell.c
+++ src/shell.c
@@ -202,7 +202,7 @@ int execute_tar_cmd(char **argv,int argc){
 
 int one_of_args_is_tar(char **argv, int argc){
   for(int i = 0; i < argc; i++){
-    if(strstr(argv[i],".tar") != NULL) return 1;
+    if(strstr(argv[i],".tar/") != NULL) return 1;
   }
   return 0;
 }
